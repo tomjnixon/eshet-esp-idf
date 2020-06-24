9d98863911fe5bf31c2e5299af5cbb37a76041bb
Thomas Nixon <tom@tomn.co.uk>
2020-06-24 10:49:56 +0100

    initial go at OTA

---
+++ include/eshet_ota.hpp
@@ -0,0 +1,104 @@
+#pragma once
+#include "eshet.hpp"
+#include <esp_ota_ops.h>
+
+namespace eshet {
+using namespace actorpp;
+
+class OTAHanderActor : public Actor {
+public:
+  OTAHanderActor(ESHETClient &client, const std::string &base)
+      : begin_chan(*this), write_chan(*this), end_chan(*this),
+        mark_valid_chan(*this), exit_chan(*this) {
+    Channel<Result> result_chan(*this);
+
+    client.action_register(base + "/begin", result_chan, begin_chan);
+    assert(std::holds_alternative<Success>(result_chan.read()));
+
+    client.action_register(base + "/write", result_chan, write_chan);
+    assert(std::holds_alternative<Success>(result_chan.read()));
+
+    client.action_register(base + "/end", result_chan, end_chan);
+    assert(std::holds_alternative<Success>(result_chan.read()));
+
+    client.action_register(base + "/mark_valid", result_chan, mark_valid_chan);
+    assert(std::holds_alternative<Success>(result_chan.read()));
+  }
+
+  void run() {
+    while (true) {
+      switch (
+          wait(exit_chan, begin_chan, write_chan, end_chan, mark_valid_chan)) {
+      case 0: {
+        return;
+      } break;
+      case 1: {
+        auto call = begin_chan.read();
+
+        update_partition = esp_ota_get_next_update_partition(NULL);
+        assert(update_partition != NULL);
+
+        esp_err_t err =
+            esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &update_handle);
+        assert(err == ESP_OK);
+
+        // XXX: return write partition so that we can query it to check if it's
+        // rebooted into it before saving
+        call.reply(Success());
+      } break;
+      case 2: {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        auto call = write_chan.read();
+        msgpack::type::raw_ref data;
+        call.convert(std::tie(data));
+
+        esp_err_t err = esp_ota_write(update_handle, data.ptr, data.size);
+        assert(err == ESP_OK);
+        call.reply(Success());
+      } break;
+      case 3: {
+        auto call = end_chan.read();
+
+        esp_err_t err = esp_ota_end(update_handle);
+        assert(err == ESP_OK);
+
+        err = esp_ota_set_boot_partition(update_partition);
+        assert(err == ESP_OK);
+
+        call.reply(Success());
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        esp_restart();
+      } break;
+      case 4: {
+        auto call = mark_valid_chan.read();
+        bool valid;
+        call.convert(std::tie(valid));
+
+        if (valid) {
+          esp_ota_mark_app_valid_cancel_rollback();
+        } else {
+          esp_ota_mark_app_invalid_rollback_and_reboot();
+          // may never return
+        }
+
+        call.reply(Success());
+      } break;
+      }
+    }
+  }
+
+  void exit() { exit_chan.push(true); }
+
+private:
+  Channel<Call> begin_chan;
+  Channel<Call> write_chan;
+  Channel<Call> end_chan;
+  Channel<Call> mark_valid_chan;
+  Channel<bool> exit_chan;
+
+  esp_ota_handle_t update_handle;
+  const esp_partition_t *update_partition;
+};
+
+using OTAHandler = ActorThread<OTAHanderActor>;
+} // namespace eshet
---
+++ src/eshet_ota_cli.cpp
@@ -0,0 +1,105 @@
+#include "CLI11.hpp"
+#include "eshet.hpp"
+#include "utils.hpp"
+#include <sstream>
+#include <string>
+
+using namespace eshet;
+
+struct error_message : public std::exception {
+  error_message(std::string message) : message(std::move(message)) {}
+  const char *what() const throw() { return message.c_str(); }
+  std::string message;
+};
+
+/// check that a result is not an error, throwing otherwise
+void check_result(const Success &r) {}
+void check_result(const Error &r) { throw error_message(r.what()); }
+
+int main(int argc, char **argv) {
+  CLI::App app{"eshet CLI"};
+
+  {
+    CLI::App *call = app.add_subcommand("flash", "flash some firmware");
+    std::string path;
+    std::string fname;
+    size_t chunk_size = 8 * 1024;
+    call->add_option("path", path)->required();
+    call->add_option("fname", fname)->required();
+    call->add_option("-c,--chunksize", chunk_size)->capture_default_str();
+    call->callback([&]() {
+      ESHETClient client(get_host_port());
+
+      std::ifstream firmware(fname, std::ios::binary);
+      std::vector<char> buf(std::istreambuf_iterator<char>(firmware), {});
+      std::cout << "size: " << buf.size() << std::endl;
+
+      Channel<Result> call_result;
+      client.action_call_pack(path + "/begin", call_result, std::make_tuple());
+      std::visit([](const auto &r) { check_result(r); }, call_result.read());
+
+      for (size_t offset = 0; offset < buf.size(); offset += chunk_size) {
+        size_t this_chunk_size = std::min(chunk_size, buf.size() - offset);
+        std::cout << "chunk " << offset << "-" << offset + this_chunk_size
+                  << std::endl;
+
+        // XXX: raw_ref doesn't own its data, so bad things can happen if this
+        // fails
+        client.action_call_pack(path + "/write", call_result,
+                                std::make_tuple(msgpack::type::raw_ref(
+                                    buf.data() + offset, this_chunk_size)));
+        std::visit([](const auto &r) { check_result(r); }, call_result.read());
+      }
+
+      client.action_call_pack(path + "/end", call_result, std::make_tuple());
+      std::visit([](const auto &r) { check_result(r); }, call_result.read());
+
+      return 0;
+    });
+  }
+
+  {
+    CLI::App *call =
+        app.add_subcommand("mark_valid", "mark the current image as valid");
+    std::string path;
+    call->add_option("path", path)->required();
+    call->callback([&]() {
+      ESHETClient client(get_host_port());
+
+      Channel<Result> call_result;
+      client.action_call_pack(path + "/mark_valid", call_result,
+                              std::make_tuple(true));
+      std::visit([](const auto &r) { check_result(r); }, call_result.read());
+      return 0;
+    });
+  }
+
+  {
+    CLI::App *call = app.add_subcommand(
+        "mark_invalid",
+        "mark the current image as invalid, reverting to the previous image");
+    std::string path;
+    call->add_option("path", path)->required();
+    call->callback([&]() {
+      ESHETClient client(get_host_port());
+
+      Channel<Result> call_result;
+      client.action_call_pack(path + "/mark_valid", call_result,
+                              std::make_tuple(false));
+      std::visit([](const auto &r) { check_result(r); }, call_result.read());
+      return 0;
+    });
+  }
+
+  app.require_subcommand(1);
+
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError &e) {
+    return app.exit(e);
+  } catch (const error_message &e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
